33d806d9fe7fa82a218554967b7fa12daecf26d5
Obed Murillo <obedmhg@gmail.com>
2019-02-18 14:13:08 -0700

    using reduce to reduce transformedIngredients to check for empty array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
         return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
             return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}  />
         });
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients</p>
+    }
 
     return (
         <div className={classes.Burger}>
