a4e1a9d2015294de7eec62b667eec68c7166f2d3
Obed Murillo <obedmhg@gmail.com>
2019-02-18 14:04:04 -0700

    adding state at BurgerBuilder to be passed to Burguer and transformed there

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,16 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+            return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}  />
+        });
+    });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="bacon"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
