bae609e59b854eada82c9c0bef48f0454ff23719
Obed Murillo <obedmhg@gmail.com>
2019-02-19 12:49:56 -0700

    adding round

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,7 +10,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        <p>Price <strong>{props.totalPrice}</strong></p>
+        <p>Price <strong>{props.totalPrice.toFixed(2)}</strong></p>
         {controls.map(ctrl => {
             return <BuildControl 
                 key={ctrl.label} 
