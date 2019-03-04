9990be9a2c84f6ea72dc7d8452090c4a788845e8
Obed Murillo <obedmhg@gmail.com>
2019-03-04 14:01:58 -0700

    Adding draft for toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux1';
-import classes from './Layout.css';
+import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 
 const laytout = (props) => (
     <Aux>
-        <div>
-            Toolbar, SideDrawer
-        </div>
+        <Toolbar></Toolbar>
         <main className={classes.Content}>
             {props.children}
         </main>
