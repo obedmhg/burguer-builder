c77b90d3264ad660a7c37ec813fccc7a8944f7ba
Obed Murillo <obedmhg@gmail.com>
2019-05-20 15:14:12 -0600

    adding react route for / and /checkout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Layout>
-              <BurgerBuilder></BurgerBuilder>
-              <Checkout></Checkout>
+              <Route path="/" exact component={BurgerBuilder}></Route>
+              <Route path="/checkout" component={Checkout}></Route>
           </Layout>
       </div>
     );
